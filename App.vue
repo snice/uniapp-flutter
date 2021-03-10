f7b40819443700012c3b131f58aeb1247317be81
zhuzhe <shaanxizhuzhe@163.com>
2021-03-10 09:25:07 +0800

    init

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+		// #ifdef APP-PLUS
+		var flutter = uni.requireNativePlugin('sn-flutter');
+		flutter.cachePages({
+			pages: ['main']
+		});
+		// #endif
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
